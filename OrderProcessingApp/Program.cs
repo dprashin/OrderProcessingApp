94643e424ae35556df918f5dc4eeec6249e6159c
dprashin <dprashin@gmail.com>
2021-09-01 21:32:12 +0200

    Extact interfaces and refactor for reducing tight coupling

--- OrderProcessingApp/Program.cs
+++ OrderProcessingApp/Program.cs
@@ -3,7 +3,9 @@
 namespace OrderProcessingApp {
     public class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            PhysicalProduct physicalProduct = new PhysicalProduct { Name = "Football", Price = 500 };
+            string physicalPaymentMessage = physicalProduct.PaymentProcessor.ProcessPayment(physicalProduct);
+            Console.WriteLine(physicalPaymentMessage + "\n");
         }
     }
 }
