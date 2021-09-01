9aef42ac7e5f0b9ad24b526cad1f04739f9732e1
dprashin <dprashin@gmail.com>
2021-09-02 00:16:57 +0200

    Implement email sender

---
+++ OrderProcessingApp/Email/EmailSender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderProcessingApp.Email {
+    public class EmailSender : IEmailSender {
+
+        public string SendEmail(string message) {
+
+            return message;
+        }
+    }
+}
