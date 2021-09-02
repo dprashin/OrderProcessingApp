7f791627f2be9bfa2e3b2328097f26daa7b03e46
unknown <Devendra Prashin@PC1678.nordic.local>
2021-09-02 18:18:08 +0200

    Add unit test for EmailSender

---
+++ OrderProcessingAppTests/EmailSenderTest.cs
@@ -0,0 +1,37 @@
+﻿using OrderProcessingApp.Email;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace OrderProcessingAppTests {
+    public class EmailSenderTest {
+        [Fact]
+        public void SendEmail_Should_Send_Email_With_Empty_Message() {
+            //Arrange
+            EmailSender emailSender = new EmailSender();
+
+            string expected = "";
+
+            //Act
+            string actual = emailSender.SendEmail("");
+
+            //Act
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SendEmail_Should_Send_Email_With_Success_Message() {
+            //Arrange
+            EmailSender emailSender = new EmailSender();
+
+            string expected = "Email sent.";
+
+            //Act
+            string actual = emailSender.SendEmail("Email sent.");
+
+            //Act
+            Assert.Equal(expected, actual);
+        }
+    }
+}
