5fe5e77c55e159105e5ffcb51de5877884b939b2
unknown <Devendra Prashin@PC1678.nordic.local>
2021-09-02 20:09:21 +0200

    Implement learn to ski paymnet type

---
+++ OrderProcessingApp/Order Processers/VideoProductOrderProcessor.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderProcessingApp.Models {
+    public class VideoProductOrderProcessor : IOrderProcessor {
+        public string ProcessPayment(IProduct product) {
+            string message = $"Generating a packing slip for - {product.Name}.";
+            if (product.Name == "Learning to Ski") {
+                message += " Congratuations! A free is \"First Aid\" is also added.";
+            }
+            return message;
+        }
+    }
+}
---
+++ OrderProcessingAppTests/VideoProductOrderProcessorTest.cs
@@ -0,0 +1,36 @@
+﻿using OrderProcessingApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace OrderProcessingAppTests {
+    public class VideoProductOrderProcessorTest {
+
+        [Fact]
+        public void PaymentProcesser_Should_Generate_Packing_Slip_Learning_To_Ski_Payment() {
+            //Arrange
+            VideoProduct videoProduct = new VideoProduct { Name = "Learning to Ski", Price = 5000 };
+            string expected = "Generating a packing slip for - Learning to Ski. Congratuations! A free is \"First Aid\" is also added.";
+
+            //Act
+            string actual = videoProduct.PaymentProcessor.ProcessPayment(videoProduct);
+
+            //Act
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void PaymentProcesser_Should_Generate_Packing_Slip_For_Learning_To_Dance_Payment() {
+            //Arrange
+            VideoProduct videoProduct = new VideoProduct { Name = "Learning to Dance", Price = 5000 };
+            string expected = "Generating a packing slip for - Learning to Dance.";
+
+            //Act
+            string actual = videoProduct.PaymentProcessor.ProcessPayment(videoProduct);
+
+            //Act
+            Assert.Equal(expected, actual);
+        }
+    }
+}
